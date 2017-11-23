4bfbe50d688da26ccfc29d239e1db5a7038eaa3b
student <student@prima.ttknn.net>
2017-11-23 14:24:39 +0300

    Initial Commit

---
+++ testProjectVadimTests/testProjectVadimTests.swift
@@ -0,0 +1,36 @@
+//
+//  testProjectVadimTests.swift
+//  testProjectVadimTests
+//
+//  Created by student on 23.11.2017.
+//  Copyright © 2017 Vadim Sokolov. All rights reserved.
+//
+
+import XCTest
+@testable import testProjectVadim
+
+class testProjectVadimTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
